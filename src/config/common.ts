efa3570087a9427c27b2a2e4cc4e92885ee01022
jesse-kaufman <jesse@jessekaufman.com>
2025-12-13 16:00:20 -0600

    chore: add initial configuration

---
+++ src/config/common.ts
@@ -0,0 +1,28 @@
+/** @file Configuration options. */
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+import dotenv from "dotenv"
+
+/** Shorthand for process.env.NODE_ENV. */
+// eslint-disable-next-line import/exports-last
+export const NODE_ENV = process.env.NODE_ENV || "production"
+
+// Get vars from env file based on environment.
+// - Production: .env
+// - Testing: .env.test
+// - Development: .env.development
+dotenv.config({
+  path: NODE_ENV === "production" ? ".env" : `.env.${NODE_ENV}`,
+  quiet: true,
+})
+
+/** Whether or not environment is development. */
+export const IS_DEV = NODE_ENV === "development"
+
+/** API URL prefix. */
+export const API_PREFIX = process.env.API_PREFIX ?? `/api/v1`
+
+/** Default port for API. */
+export const PORT = Number(process.env.PORT) || 3000
+
+/** Log level for logging; defaults to "info". */
+export const LOG_LEVEL = process.env.LOG_LEVEL || "info"
