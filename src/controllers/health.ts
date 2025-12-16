351c30ef4a3f692c422897dc60efd175619ad2cc
jesse-kaufman <jesse@jessekaufman.com>
2025-12-15 19:51:41 -0600

    docs: add jsdoc to health controller

--- src/controllers/health.ts
+++ src/controllers/health.ts
@@ -1,7 +1,11 @@
 /** @file API health controller. */
-import { type RequestHandler } from "express"
+import type { RequestHandler } from "express"
 
-// Health check endpoint
+/**
+ * Outputs healthcheck.
+ * @param req - Request object.
+ * @param res - Response object.
+ */
 const getHealth: RequestHandler = (req, res) => {
   res.json({ status: "ok" })
 }
