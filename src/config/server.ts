5a4c370cc442099073047b01391ff0231110b227
jesse-kaufman <jesse@jessekaufman.com>
2025-12-16 21:39:52 -0600

    fix: use --env-file argument for tsx instead of dotenv

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,21 +1,10 @@
 /** @file Configuration options. */
-import dotenv from "dotenv"
 
 /** API version for use in API prefix. */
 const API_VERSION = process.env.API_VERSION || 1
 
 /** Shorthand for process.env.NODE_ENV. */
-// eslint-disable-next-line import/exports-last
 export const NODE_ENV = process.env.NODE_ENV || "production"
-
-// Get vars from env file based on environment.
-// - Production: .env
-// - Testing: .env.test
-// - Development: .env.development
-dotenv.config({
-  path: NODE_ENV === "production" ? ".env" : `.env.${NODE_ENV}`,
-  quiet: true,
-})
 
 /** API URL prefix. */
 export const API_PREFIX = `${process.env.API_PREFIX || "/api"}/v${API_VERSION}`
